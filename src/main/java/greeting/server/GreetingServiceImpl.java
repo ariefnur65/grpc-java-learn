ddc4a9534c81a5e8af46ef18c191c2d1c6e2473b
arief.prakoso <ariefnur652010@gmail.com>
2022-07-31 05:45:38 +0700

    (Master) - add greeting server implementation

---
+++ src/main/java/greeting/server/GreetingServiceImpl.java
@@ -0,0 +1,18 @@
+package greeting.server;
+
+import com.proto.greeting.GreetingRequest;
+import com.proto.greeting.GreetingResponse;
+import com.proto.greeting.GreetingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+    @Override
+    public  void greet(GreetingRequest request, StreamObserver<GreetingResponse> responseStreamObserver){
+        String greetingBandung = "Halo Bandung, Nama Saya " + request.getFirstName();
+        GreetingResponse greetingResponse = GreetingResponse
+                .newBuilder()
+                .setResult(greetingBandung)
+                .build();
+        responseStreamObserver.onNext(greetingResponse);
+    }
+}
