90426ab72958525480cee7820b064f21156c1c52
arief.prakoso <ariefnur652010@gmail.com>
2022-07-31 06:40:13 +0700

    (Master) - add greeting service implementation onComplete

--- src/main/java/greeting/server/GreetingServiceImpl.java
+++ src/main/java/greeting/server/GreetingServiceImpl.java
@@ -14,5 +14,6 @@ public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImpl
                 .setResult(greetingBandung)
                 .build();
         responseStreamObserver.onNext(greetingResponse);
+        responseStreamObserver.onCompleted();
     }
 }
