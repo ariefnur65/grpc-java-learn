39d0d563f2780bc15072c18191d45f5b54097b69
Arief Nur Prakoso <arief.prakoso@btpn.com>
2023-05-03 06:19:14 +0700

    (main) - add greeting server main class

---
+++ src/main/java/greeting/server/GreetingServer.java
@@ -0,0 +1,23 @@
+package greeting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int PORT = 5051;
+        Server server = ServerBuilder.forPort(PORT).build();
+        server.start();
+        System.out.println("Server started");
+        System.out.println("Listening to " + PORT);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Receive shotdown request");
+            server.shutdown();
+            System.out.println("Server stopped");
+        }));
+        server.awaitTermination();
+    }
+}
